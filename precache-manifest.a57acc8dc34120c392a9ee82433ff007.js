df84b348be6a7b88158e174382b5b6d55c1db214
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-10 21:28:20 +0000

    Deploy arnabk/daily-co-demo to github.com/arnabk/daily-co-demo.git:gh-pages

---
+++ precache-manifest.a57acc8dc34120c392a9ee82433ff007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d408f0a22d20f199f964078a785ce729",
+    "url": "/daily-co-demo/index.html"
+  },
+  {
+    "revision": "a4e70ee3ec48a48dffae",
+    "url": "/daily-co-demo/static/js/2.667e0514.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/daily-co-demo/static/js/2.667e0514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75e53bbbaba6dc99d72",
+    "url": "/daily-co-demo/static/js/main.7cee6809.chunk.js"
+  },
+  {
+    "revision": "dcc5864dba483a88e22e",
+    "url": "/daily-co-demo/static/js/runtime-main.7699f09b.js"
+  }
+]);
