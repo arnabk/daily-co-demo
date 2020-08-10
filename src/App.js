732328f4f5b29df6aa73feaf821e177a1d72214d
Arnab Karmakar <arnab.k@gmail.com>
2020-08-10 15:06:19 -0600

    Demo integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import DailyIframe from "@daily-co/daily-js";
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  margin: auto;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  position: relative;
+  top: 45%;
+  width: 200px;
+  height: 50px;
+  border: none;
+  background-color: aquamarine;
+  border-radius: 10px;
+  font-size: x-large;
+  outline: none;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    border: 1px dashed black;
+  }
+`;
+
+const VideoFrame = styled.iframe`
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  width: 400px;
+  height: 500px;
+  display: ${props => props.joined ? '' : 'none'};
+`;
 
 function App() {
+  const [videoFrame, setVideoFrame] = useState();
+  const [joined, setJoined] = useState(false);
+
+  useEffect(() => {
+    setVideoFrame(DailyIframe.wrap(document.querySelector("iframe")));
+  }, []);
+
+  const toggleJoinClass = () => {
+    if (videoFrame) {
+      if (joined) {
+        videoFrame.leave();
+        setJoined(false);
+      } else {
+        videoFrame.join({ url: "https://arnabkarmakar.daily.co/test-room",
+      showLeaveButton: false, showFullscreenButton: false, });
+        setJoined(true);
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Button onClick={toggleJoinClass}>{joined ? "End" : "Join"} Class</Button>
+      <VideoFrame allow="microphone; camera; autoplay" joined={joined} />
+    </MainContainer>
   );
 }
 
